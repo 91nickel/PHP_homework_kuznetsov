046d506ca9838da9b73558c05a19724f2bce0db2
91nickel <43931371+91nickel@users.noreply.github.com>
2018-10-13 01:58:39 +0300

    Update lesson_1_1.php

--- lesson_1_1.php
+++ lesson_1_1.php
@@ -1,3 +1,36 @@
+<?php
+
+$x = rand(0,100);
+$textBox1 = 'НЕ входит в числовой ряд';
+$textBox2 = 'НЕ входит в числовой ряд';
+
+if (isset($_GET['y'])) {
+
+    $y = $_GET['y'];
+    $textBox3 = 'Число';
+
+} else {
+
+    $y = 'ПАРАМЕТР НЕ ЗАДАН';
+    $textBox2 = '';
+    $textBox3 = '';
+
+}
+
+for ($var1 = 1, $var2 = 1; $var1 < 100; $var3 = $var1, $var4 = $var4 . ' ' . $var3, $var1 = $var1 + $var2, $var2 = $var3) {
+    if ($var1 == $x) {
+        $textBox1 = 'входит в числовой ряд';
+        break;
+    }
+}
+for ($var11 = 1, $var21 = 1; $var11 < 100; $var31 = $var11, $var41 = $var41 . ' ' . $var31, $var11 = $var11 + $var21, $var21 = $var31) {
+    if ($var11 == $y) {
+        $textBox2 = 'входит в числовой ряд';
+        break;
+    }
+}
+
+?>
 <html lang='en'>
 <head>
     <meta charset='UTF-8'>
@@ -10,31 +43,11 @@
             margin-left: 3rem;
         }
     </style>
-    <?php
-        $x = rand(0,100);
-        $y = $_GET['y'];
-        $textBox1 = 'НЕ входит в числовой ряд';
-        $textBox2 = 'НЕ входит в числовой ряд';
-
-    echo "Х = $x ; Y = $y <br /> Последовательность:  ";
-
-    for ($var1 = 1, $var2 = 1; $var1 < 100; $var3 = $var1, $var1 = $var1 + $var2, $var2 = $var3) {
-        echo "$var1  ";
-        if ($var1 == $x) {
-            $textBox1 = 'входит в числовой ряд';
-            break;
-    }
-}
-    for ($var11 = 1, $var21 = 1; $var11 < 100; $var31 = $var11, $var11 = $var11 + $var21, $var21 = $var31) {
-            if ($var11 == $y) {
-            $textBox2 = 'входит в числовой ряд';
-            break;
-        }
-    }
-?>
 </head>
 <body>
 <h1>Дополнительное задание к лекции 1.1 «Введение в PHP»</h1>
+<p><?php echo "Х = $x ; Y = $y;" ?></p>
+<p>Последовательность: <?php echo $var4 ?></p>
 <h2>Вариант выполнения задания с использованием функции rand(1,100)</h2>
 <div>
     <?php
@@ -46,7 +59,7 @@
 <p>Ввод числа пользователем осуществляется через адресную строку в виде .../lesson_1_1.php?y=13. По умолчанию задано число '13'.</p>
 <div>
     <?php
-    echo "Число  $y  $textBox2";
+    echo "$textBox3  $y  $textBox2";
     ?>
 </div>
 </body>
