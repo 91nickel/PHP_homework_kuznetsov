3ed7a23417b96719baefeb9b646a57da23ecd225
91nickel <43931371+91nickel@users.noreply.github.com>
2018-10-28 12:52:33 +0300

    Update L5list.php

--- L5list.php
+++ L5list.php
@@ -1,1 +1,22 @@
-1.json
+<?php
+$dir    = 'tests';
+$files1 = scandir($dir);
+$files2 = scandir($dir, 1);
+$i = 0;
+//var_dump($files2);
+echo 'ВЫБЕРИТЕ ДОСТУПНЫЙ ТЕСТ'."<br />";
+?>
+
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Список загруженных тестов. Задание 5.</title>
+</head>
+<body>
+<?php foreach ($files2 as $item) : ?>
+    <?php $i++;
+    if ($i > count($files2) - 2) {break;} ?>
+    <a href="<?php echo 'L5test.php?y='.$i; ?>"><?php echo 'Тест №'.$i; ?></a><br>
+<?php endforeach; ?>
+</body>
+</html>
